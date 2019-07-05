a336779b42ed099b4c7559f7b62e6c17ff6a855a
altorresv <atorresv1812@hotmail.com>
2019-07-05 16:07:54 -0500

    Primer commit

---
+++ src/hilo/Hilo.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hilo;
+
+/**
+ *
+ * @author ASUS_X455L
+ */
+public class Hilo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
